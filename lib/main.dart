73e33f516edba9815d6e461fd25682c4268344a9
Kritbovorn <ktaweeyossak@yahoo.com>
2022-03-23 12:04:10 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,363 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Login Web',
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: const Color(0xFFF5F5F5),
+      body: ListView(
+        children: [
+          const Menu(),
+          Container(
+              margin: const EdgeInsets.symmetric(vertical: 35),
+              child: const Content()),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Row(
+              children: [
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _menuItem("Home"),
+                      _menuItem('About Us'),
+                      _menuItem('Contact'),
+                      _menuItem('Help'),
+                    ],
+                  ),
+                ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _menuItem('Sign In', isActive: true),
+                      _registerButton(),
+                    ],
+                  ),
+                ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+              ],
             ),
-          ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _menuItem(String title, {bool isActive = false}) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: isActive ? Colors.deepPurple : Colors.grey,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        isActive
+            ? Container(
+                width: 24,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              )
+            : const SizedBox(),
+      ],
+    );
+  }
+
+  Widget _registerButton() {
+    return Container(
+      // width: 30,
+      // height: 8,
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 230, 228, 228),
+              spreadRadius: 4,
+              blurRadius: 4,
+            ),
+          ]),
+      child: const Text(
+        'Register',
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black54),
+      ),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  const Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Expanded(
+          child: SizedBox(),
+        ),
+        Expanded(
+          flex: 8,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Sign In to\nMy Application',
+                        style: TextStyle(
+                            fontSize: 45, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        "If you don't have an account",
+                        style: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: const [
+                          Text(
+                            "You can",
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            'Register Here !',
+                            style: TextStyle(
+                                color: Colors.deepPurple,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Image.asset(
+                        'assets/images/illustration-2.png',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(34.0),
+                  child: Image.asset(
+                    'assets/images/illustration-1.png',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: _formLogin(),
+              )
+            ],
+          ),
+        ),
+        const Expanded(
+          child: SizedBox(),
+        ),
+      ],
+    );
+  }
+
+  Widget _formLogin() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Email or Phone number',
+              fillColor: Colors.blueGrey[50],
+              filled: true,
+              labelStyle: const TextStyle(fontSize: 10),
+              contentPadding: const EdgeInsets.only(left: 30),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.blueGrey),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Password',
+              counterText: 'Forgot Password?',
+              suffixIcon: const Icon(
+                Icons.visibility_off_outlined,
+                color: Colors.grey,
+              ),
+              fillColor: Colors.blueGrey[50],
+              filled: true,
+              labelStyle: const TextStyle(fontSize: 10),
+              contentPadding: const EdgeInsets.only(left: 30),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.blueGrey),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blue,
+              onPrimary: Colors.white,
+              shadowColor: Colors.greenAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(32),
+              ),
+              minimumSize: const Size(double.infinity, 55),
+            ),
+            child: const Text('Sign In'),
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: const [
+              Expanded(
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('Or continue with'),
+              ),
+              Expanded(
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _loginWithButton('assets/images/google.png', isActive: true),
+              _loginWithButton('assets/images/github.png', isActive: true),
+              _loginWithButton('assets/images/facebook.png', isActive: true),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _loginWithButton(String image, {bool isActive = false}) {
+    return Container(
+      width: 80,
+      height: 60,
+      decoration: isActive
+          ? BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 218, 216, 216),
+                    spreadRadius: 4,
+                    blurRadius: 5,
+                  )
+                ]
+              
+              )
+          : BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.grey),
+            ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Image.asset(
+          image,
+          // fit: BoxFit.fitWidth,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
