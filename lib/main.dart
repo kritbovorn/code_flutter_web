21451b7d122bb396abb90081b53d240ff54f438a
Kritbovorn <ktaweeyossak@yahoo.com>
2022-04-11 20:27:53 +0700

    finish SliverAppBar()

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,27 @@
+import 'package:beginner/config/palette.dart';
+import 'package:beginner/screens/screens.dart';
 import 'package:flutter/material.dart';
 
-  void main() {
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = "Flutter";
+  static const String _title = "Facebook UI";
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: _title,
-      home: HomePage(title: "Flutter"),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-
-  final String title;
-
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(  
-        title: Text(widget.title),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
       ),
-      body: Container(),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:beginner/config/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            backgroundColor: Colors.white,
+            title: const Text(
+              'facebook',
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(icon: Icons.search, iconSize: 28, onPressed: () {}),
+              CircleButton(
+                  icon: MdiIcons.facebookMessenger,
+                  iconSize: 28,
+                  onPressed: () {}),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
