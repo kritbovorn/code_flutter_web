4231a83aca12564086d7973d340d2befb930f951
Kritbovorn <ktaweeyossak@yahoo.com>
2022-04-14 16:36:22 +0700

    Add assets and pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,22 @@
 import 'package:flutter/material.dart';
 
-  void main() {
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = "Flutter";
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: _title,
-      home: HomePage(title: "Flutter"),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-
-  final String title;
-
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(  
-        title: Text(widget.title),
+      title: 'Netflix UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Container(),
+      home: const Scaffold(),
     );
   }
 }
