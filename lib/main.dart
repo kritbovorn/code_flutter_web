b1c182ba4a4ad7d63096e5065f880823d2cd58e7
Kritbovorn <ktaweeyossak@yahoo.com>
2022-04-18 23:07:44 +0700

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,30 @@
+// https://www.youtube.com/watch?v=CSa6Ocyog4U&list=PLzOt3noWLMthJKm8SJl2zmUlJiZp7fzo7&index=2
+
+import 'package:beginner/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-  void main() {
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = "Flutter";
+  static const String _title = "Flutter Travel UI";
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: _title,
-      home: HomePage(title: "Flutter"),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-
-  final String title;
-
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(  
-        title: Text(widget.title),
+      theme: ThemeData(
+        primaryColor: const Color(0xff3ebace),
+        scaffoldBackgroundColor: const Color(0xfff3f5f7),
+        colorScheme: ThemeData()
+            .colorScheme
+            .copyWith(secondary: const Color(0xffd8ecf1)),
       ),
-      body: Container(),
+      home: const HomeScreen(),
     );
   }
 }
