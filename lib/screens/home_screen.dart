06d96c8c586e904e82b9504452ec1ca964fe773e
Kritbovorn <ktaweeyossak@yahoo.com>
2022-04-19 09:18:11 +0700

    12.00: Four Icon()

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,8 +9,67 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.personWalking,
+    FontAwesomeIcons.personBiking,
+  ];
+
+  Widget _buildIconButton(int index) {
+    return GestureDetector(
+      onTap: () => setState(() => _selectedIndex = index),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 30),
+        width: 60.0,
+        height: 60.0,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).colorScheme.secondary
+              : const Color(0xffe7ebee),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 24,
+          color: _selectedIndex == index
+              ? const Color.fromARGB(255, 94, 195, 17)
+              : Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 60),
+              child: Text(
+                'What do you would like to find?',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map((e) => _buildIconButton(e.key))
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
