b2911b48a9ab796e6667354010db5437d583d356
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-03 21:17:03 +0700

    4:01:38: finish layout UI & will continued to make collaspe text when click TextButton

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,6 +1,9 @@
+import 'package:beginner/utils/colors.dart';
 import 'package:beginner/widgets/app_column.dart';
 import 'package:beginner/utils/dimensions.dart';
 import 'package:beginner/widgets/app_icon.dart';
+import 'package:beginner/widgets/big_text.dart';
+import 'package:beginner/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class PopularFoodDetail extends StatefulWidget {
@@ -16,7 +19,7 @@ class _PopularFoodDetailState extends State<PopularFoodDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(250, 250, 250, 250),
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           SizedBox(
@@ -28,6 +31,7 @@ class _PopularFoodDetailState extends State<PopularFoodDetail> {
             ),
           ),
           SafeArea(
+            bottom: false,
             child: SizedBox(
               child: Column(
                 children: [
@@ -57,18 +61,111 @@ class _PopularFoodDetailState extends State<PopularFoodDetail> {
                           topRight: Radius.circular(Dimensions.height20),
                         ),
                       ),
-                      child: AppColumn(
-                        title: 'Chinese Side',
-                        numberOfLiked: numberOfLiked,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          AppColumn(
+                            title: 'Chinese Side',
+                            numberOfLiked: numberOfLiked,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(Dimensions.height20),
+                            child: const BigText(text: 'Introduce'),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: Dimensions.height15,
+                              bottom: Dimensions.height15,
+                              left: Dimensions.height20,
+                              right: Dimensions.height20,
+                            ),
+                            child: SmallText(
+                              size: Dimensions.height18,
+                              text:
+                                  'Chicken marinated in a spiced yoghurt is placed in a large pot, then layered with fried onions (cheeky easy sub below !), fresh coriander/cilantro , then par boiled I...',
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: Dimensions.height20),
+                            child: Row(
+                              children: [
+                                BigText(
+                                  text: 'Show more',
+                                  size: Dimensions.height18,
+                                  color: AppColors.mainColor,
+                                ),
+                                const Icon(Icons.arrow_drop_down_sharp),
+                              ],
+                            ),
+                          )
+                        ],
                       ),
                     ),
                   ),
-                  
                 ],
               ),
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.fromLTRB(
+          Dimensions.width20,
+          Dimensions.height30,
+          Dimensions.width20,
+          Dimensions.height30,
+        ),
+        height: Dimensions.height120,
+        decoration: BoxDecoration(
+          color: AppColors.buttonBackgroundColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(Dimensions.width20 * 2),
+            topRight: Radius.circular(Dimensions.width20 * 2),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              padding: EdgeInsets.all(Dimensions.height20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+              ),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.remove,
+                    color: AppColors.signColor,
+                  ),
+                  SizedBox(
+                    width: Dimensions.width10 / 2,
+                  ),
+                  const BigText(text: '0'),
+                  SizedBox(
+                    width: Dimensions.width10 / 2,
+                  ),
+                  const Icon(
+                    Icons.add,
+                    color: AppColors.signColor,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(Dimensions.height20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: AppColors.mainColor,
+              ),
+              child: const BigText(
+                text: '\$10 | Add to cart',
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
