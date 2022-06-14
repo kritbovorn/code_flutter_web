c566ed22ee46e5e050e8aa5f61c9c6560a72bc77
Kritbovorn <ktaweeyossak@yahoo.com>
2022-06-14 10:27:59 +0700

    TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,49 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: _title,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.purple,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+            backgroundColor: Colors.purple,
+            side: const BorderSide(color: Colors.orange, width: 2),
+            shape:
+                BeveledRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            shadowColor: Colors.pink,
+            elevation: 10,
+          ),
+        ),
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            TextButton(
+              child: const Text('Submit'),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.shopping_cart),
+              label: const Text('Shopping Cart'),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.shopping_cart),
+              label: const Text('Shopping Cart'),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.purple,
+                side: const BorderSide(color: Colors.orange, width: 2),
+                shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                shadowColor: Colors.pink,
+                elevation: 10,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
