a9462c9f338713cc96bf3433ec1b870822b6f2ab
Kritbovorn <ktaweeyossak@yahoo.com>
2022-06-23 19:12:14 +0700

    finish

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  bool bookmarked = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 6),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+  }
+
+  action() {
+    if (bookmarked == false) {
+      _controller.forward();
+      bookmarked = true;
+    } else {
+      _controller.reverse();
+      bookmarked = false;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () => action(),
+          child: Lottie.network(
+            'https://assets6.lottiefiles.com/packages/lf20_teutzxdt.json',
+            // controller: _controller
+          ),
+        ),
+      ),
+    );
   }
 }
